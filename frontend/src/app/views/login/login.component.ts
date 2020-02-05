88336f9f20f091822fcfc9fc4e6447532c617a6b
pilar <pilariuxy.xelera96@gmail.com>
2020-02-05 21:24:13 +0100

    Ajustes login y registrar

--- frontend/src/app/views/login/login.component.ts
+++ frontend/src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,4 +13,24 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+
+    // if (this.loginForm.invalid) {
+    //   this.loginForm.setErrors({ login: literal.errors.emptyForms });
+    //   return;
+    // }
+    // this.loading = true;
+
+    // setTimeout(() => {
+    //   this.serviceLogin.checkLogin().subscribe(data => {
+    //     this.serviceLogin.setDatosForm(data);
+    //     localStorage.setItem('userData', JSON.stringify(data));
+    //     if (data.user === this.loginForm.get('user').value && data.pass === +this.loginForm.get('pass').value) {
+    //       this.route.navigateByUrl('/main');
+    //     } else {
+    //       this.loginForm.setErrors({ login: literal.errors.login });
+    //     }
+    //     this.loading = false;
+    //   });
+    // }, 1000);
+  
 }
