d3e484cf01b3ef49467a1c82fb1b55e9691264df
pilar <pilariuxy.xelera96@gmail.com>
2020-02-20 19:09:35 +0100

    Validaciones password

--- frontend/src/app/views/registrar/registrar.component.ts
+++ frontend/src/app/views/registrar/registrar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Usuario, Poblacion } from 'src/app/modelos/modelos';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Usuario, Poblacion, Provincia } from 'src/app/modelos/modelos';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { PoblacionService } from 'src/app/services/poblacion.service';
+import { ProvinciaService } from 'src/app/services/provincia.service';
+import { MustMatch } from 'src/app/utilidades/validaciones';
 
 @Component({
   selector: 'app-registrar',
@@ -15,13 +17,17 @@ export class RegistrarComponent implements OnInit {
   public formuser: FormGroup;
   public usuarios: Usuario;
   public poblaciones: Poblacion;
+  public provincias: Provincia;
+
   private patron = '[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}';
 
   constructor( private router: Router, private formBuilder: FormBuilder,
-               private usuarioService: UsuarioService, private poblacionService: PoblacionService) {
+               private usuarioService: UsuarioService, private poblacionService: PoblacionService,
+               private provinciaService: ProvinciaService) {
     this.formuser = formBuilder.group({
       email: ['', [Validators.required,  Validators.pattern(this.patron)]],
       password1: ['', [Validators.required, Validators.minLength(4)]],
+      password2: ['', [Validators.required]],
       apellidos: [''],
       nombre: [''],
       dni: [''],
@@ -30,15 +36,20 @@ export class RegistrarComponent implements OnInit {
       imagen: [''],
       poblacion: [''],
       provincia: [''],
-      cp: [''],
-      password2: ['']
+      cp: ['']
+    }, {
+      validator: MustMatch('password1', 'password2')
     });
+
+    // this.formuser.get('password2').setValidators(
+    //   CustomValidators.equals(this.formuser.get('password1'))
+    // );
   }
   ngOnInit() {
-    this.poblacionService.getPoblaciones().subscribe(
+    this.provinciaService.getProvincias().subscribe(
       res => {
         console.log(res);
-        this.poblaciones = res;
+        this.provincias = res;
       },
       err => {
         console.log(err);
@@ -61,7 +72,38 @@ export class RegistrarComponent implements OnInit {
     return this.formuser.get('email');
   }
 
-  get password() {
-    return this.formuser.get('password');
+  get password1() {
+    return this.formuser.get('password1');
   }
+
+  get password2() {
+    return this.formuser.get('password2');
+  }
+
+  public onOptionsSelected(value: string ) {
+    // console.log('the selected value is ' + value);
+    this.poblacionService.getPoblaciones().subscribe(
+      res => {
+        console.log(res);
+        this.poblaciones = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  // private validatePassword(control: AbstractControl) {
+  //   const password2 = control.value;
+  //   let password1 = document.getElementById("input1").value;
+
+  //   if( password2 === password1) {
+  //       console.log('Los valores son iguales');
+  //   } else {
+  //       console.log('los valores no son iguales');
+  //   }
+  //   return error;
+  // }
+
+
 }
