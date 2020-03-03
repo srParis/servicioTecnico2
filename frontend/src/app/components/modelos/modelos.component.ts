a3f7e0e6f8a32dc9d98461a7400fc7eab49a6ba5
srParis <edubskt7@gmail.com>
2020-03-03 19:05:44 +0100

    formulario modelos

--- frontend/src/app/components/modelos/modelos.component.ts
+++ frontend/src/app/components/modelos/modelos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MarcaService } from 'src/app/services/marcas.service';
 import { ModeloService } from 'src/app/services/modelo.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Modelo } from 'src/app/modelos/modelos';
+import { Modelo, Marca } from 'src/app/modelos/modelos';
 
 @Component({
   selector: 'app-modelos',
@@ -10,17 +10,35 @@ import { Modelo } from 'src/app/modelos/modelos';
   styleUrls: ['./modelos.component.scss']
 })
 export class ModelosComponent implements OnInit {
-  private formmarc: FormGroup;
+  private formmodelo: FormGroup;
   public modelos: Modelo;
+  public id_marcas: Marca;
 
-
-  constructor(private formBuilder: FormBuilder, private modeloService: ModeloService) { }
+  constructor(private formBuilder: FormBuilder, private modeloService: ModeloService, private marcasService: MarcaService) {
+    this.formmodelo = formBuilder.group({
+      nombre: [],
+      modelo: [],
+      id_marca: []
+    });
+   }
 
   ngOnInit() {
     this.modeloService.getModelos().subscribe(
       res => {
         console.log(res);
         this.modelos = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  public onOptionsSelected(value: number) {
+    // console.log('the selected value is ' + value);
+    this.marcasService.getMarcas().subscribe(
+      res => {
+        console.log(res);
+        this.id_marcas = res;
       },
       err => {
         console.log(err);
