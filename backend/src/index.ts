485a87abbd34c6c4f9d06459b125ee38bd427d49
srParis <edubskt7@gmail.com>
2020-07-05 01:19:20 +0200

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,6 @@
 import express, {Application} from 'express';
 import indexRoutes from './routes/indexRoutes';
 import usuariosRoutes from './routes/usuariosRoutes';
-
-// falta direccion
 import direccionesRoutes from './routes/direccionesRoutes';
 import dispositivosRoutes from './routes/dispositivosRoutes';
 import empresasRoutes from './routes/empresasRoutes';
@@ -21,7 +19,6 @@ import tipoReparacionRoutes from './routes/tipoReparacionRoutes';
 import trabajadoresRoutes   from './routes/trabajadoresRoutes';
 import morgan from 'morgan';
 import cors from 'cors';
-
 class Server {
     public app:Application;
     constructor() {
@@ -29,14 +26,13 @@ class Server {
         this.config();
         this.routes();
     }
-
+    
     config():void{
         this.app.set('port', process.env.PORT||3000);
         this.app.use(morgan('dev'));
         this.app.use(cors());
         this.app.use(express.json());
     }
-
     routes():void{
         this.app.use(indexRoutes);
         this.app.use('/usuarios',usuariosRoutes);
@@ -56,14 +52,10 @@ class Server {
         this.app.use('/tipoEstado', tipoEstadoRoutes);
         this.app.use('/tipoReparacion',tipoReparacionRoutes);
         this.app.use('/trabajadores', trabajadoresRoutes);
-    
-
     }
-
     start():void{
         this.app.listen(this.app.get('port'), () => console.log('El servidor esta escuchando en el puerto ', this.app.get('port')));
     }
 }
-
 const server = new Server();
 server.start();
